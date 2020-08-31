dc464e8ad49faa02816da1791640591761bb3a19
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2020-08-31 14:21:54 +0200

    iterating mosaic creation

--- python/example_mosaic_gamma_VH.py
+++ python/example_mosaic_gamma_VH.py
@@ -12,170 +12,54 @@ import os
 from pyroSAR.ancillary import groupbyTime, seconds
 from spatialist import stack
 from spatialist.ancillary import finder
+from pyroSAR.drivers import identify
 
+filename = "F:/geodata/geo402/S2/xx_S2_indices/stack_evi/S2A_MSIL1C_20151229T075332_N0201_R135_T35JMH_mskd.vrt"
+
+from pyroSAR import identify
+id = identify(filename)
+print(id.outname_base())
 
 def main():
-    # define input directory containing file sto be stacked
-    dir_in = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/02_processed_gamma/Free State/proc_out/'
-    
-    # define output file name
-    dstfile = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/03_stacks/S1_A_D_VH_stack_base_period_03_2015_03_2018_free_state_tile_1'
-    
-    # shapefile (for stack boundaries)
-    shp = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/xx_study_area/SALDI_06_Free_State_fishnet_tile_1.shp'
-    
-    # store results in separate files or one single stack file? If separate then dstfile is used as a directory.
-    sep = False
-    
-    # list files to be resampled; those not overlapping with the shapefile geometry will excluded by function stack
-    srcfiles = finder(dir_in, ['S1*2015*VH*db.tif'])
-    srcfiles.extend(finder(dir_in, ['S1*2016*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*2017*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201801*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201802*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201803*VH*db.tif']))
-    print(dstfile)
-    # check whether dstfile is already a file
-    if os.path.isfile(dstfile):
-        raise IOError('dstfile already exists')
-    
-    # create groups of similar time stamps for mosaicking.
-    # All images with a time stamp of less than 30s difference will be grouped
-    groups = groupbyTime(srcfiles, seconds, 30)
 
-    # final function call
-    # groups will be mosaicked first
-    # the resulting images will all have the same extent
-    stack(srcfiles=groups, dstfile=dstfile, resampling='bilinear',
-          targetres=[10, 10], srcnodata=-99, dstnodata=-99,
-          shapefile=shp, sortfun=seconds, separate=sep, overwrite=False)
+    dirs = ["stack_evi", "stack_ndvi", "stack_msavi", "stack_reip", "stack_rvi"]
+
+    # shapefile (for stack boundaries)
+    shp = 'F:/geodata/geo402/S2/xx_S2_indices/##study_area/LADYBRAND_final_enlarged_study_area.shp'
+
+    # store results in separate files or one single stack file? If separate then dstfile is used as a directory.
+    sep = True
+
+    for dir in dirs:
+
+        # define input directory containing files to be stacked
+        dir_in = 'F:/geodata/geo402/S2/xx_S2_indices/' + dir
+        print(dir_in)
+
+        # define output file name
+        dstfile = 'F:/geodata/geo402/S2/xx_S2_indices/mosaics/' + dir + '.vrt'
+
+        # list files to be resampled; those not overlapping with the shapefile geometry will excluded by function stack
+        srcfiles = finder(dir_in, ['*'])
+
+        print(dstfile)
+
+        # check whether dstfile is already a file
+        if os.path.isfile(dstfile):
+            raise IOError('dstfile already exists')
+    
+        # create groups of similar time stamps for mosaicking.
+        # All images with a time stamp of less than 30s difference will be grouped
+        groups = groupbyTime(srcfiles, seconds, 30)
+
+        # final function call
+        # groups will be mosaicked first
+        # the resulting images will all have the same extent
+        stack(srcfiles=groups, dstfile=dstfile, resampling='bilinear',
+              targetres=[30, 30], srcnodata=-99, dstnodata=-99,
+              shapefile=shp, sortfun=seconds, separate=sep, overwrite=False)
 
 
 if __name__ == '__main__':
     main()
-
-def main():
-    # define input directory containing file sto be stacked
-    dir_in = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/02_processed_gamma/Free State/proc_out/'
-    
-    # define output file name
-    dstfile = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/03_stacks/S1_A_D_VH_stack_base_period_03_2015_03_2018_free_state_tile_2'
-    
-    # shapefile (for stack boundaries)
-    shp = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/xx_study_area/SALDI_06_Free_State_fishnet_tile_2.shp'
-    
-    # store results in separate files or one single stack file? If separate then dstfile is used as a directory.
-    sep = False
-    
-    # list files to be resampled; those not overlapping with the shapefile geometry will excluded by function stack
-    srcfiles = finder(dir_in, ['S1*2015*VH*db.tif'])
-    srcfiles.extend(finder(dir_in, ['S1*2016*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*2017*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201801*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201802*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201803*VH*db.tif']))
-    print(dstfile)
-    # check whether dstfile is already a file
-    if os.path.isfile(dstfile):
-        raise IOError('dstfile already exists')
-    
-    # create groups of similar time stamps for mosaicking.
-    # All images with a time stamp of less than 30s difference will be grouped
-    groups = groupbyTime(srcfiles, seconds, 30)
-
-    # final function call
-    # groups will be mosaicked first
-    # the resulting images will all have the same extent
-    stack(srcfiles=groups, dstfile=dstfile, resampling='bilinear',
-          targetres=[10, 10], srcnodata=-99, dstnodata=-99,
-          shapefile=shp, sortfun=seconds, separate=sep, overwrite=False)
-
-
-if __name__ == '__main__':
-    main()
-	
-
-def main():
-    # define input directory containing file sto be stacked
-    dir_in = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/02_processed_gamma/Free State/proc_out/'
-    
-    # define output file name
-    dstfile = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/03_stacks/S1_A_D_VH_stack_base_period_03_2015_03_2018_free_state_tile_3'
-    
-    # shapefile (for stack boundaries)
-    shp = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/xx_study_area/SALDI_06_Free_State_fishnet_tile_3.shp'
-    
-    # store results in separate files or one single stack file? If separate then dstfile is used as a directory.
-    sep = False
-    
-    # list files to be resampled; those not overlapping with the shapefile geometry will excluded by function stack
-    srcfiles = finder(dir_in, ['S1*2015*VH*db.tif'])
-    srcfiles.extend(finder(dir_in, ['S1*2016*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*2017*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201801*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201802*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201803*VH*db.tif']))
-    print(dstfile)
-    # check whether dstfile is already a file
-    if os.path.isfile(dstfile):
-        raise IOError('dstfile already exists')
-    
-    # create groups of similar time stamps for mosaicking.
-    # All images with a time stamp of less than 30s difference will be grouped
-    groups = groupbyTime(srcfiles, seconds, 30)
-
-    # final function call
-    # groups will be mosaicked first
-    # the resulting images will all have the same extent
-    stack(srcfiles=groups, dstfile=dstfile, resampling='bilinear',
-          targetres=[10, 10], srcnodata=-99, dstnodata=-99,
-          shapefile=shp, sortfun=seconds, separate=sep, overwrite=False)
-
-
-if __name__ == '__main__':
-    main()
-	
-
-def main():
-    # define input directory containing file sto be stacked
-    dir_in = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/02_processed_gamma/Free State/proc_out/'
-    
-    # define output file name
-    dstfile = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/06_Free_State/03_stacks/S1_A_D_VH_stack_base_period_03_2015_03_2018_free_state_tile_4'
-    
-    # shapefile (for stack boundaries)
-    shp = '/geonfs01_vol1/01_EMS_SALDI_KNP_S1/01_S1_GRD/02_SALDI/xx_study_area/SALDI_06_Free_State_fishnet_tile_4.shp'
-    
-    # store results in separate files or one single stack file? If separate then dstfile is used as a directory.
-    sep = False
-    
-    # list files to be resampled; those not overlapping with the shapefile geometry will excluded by function stack
-    srcfiles = finder(dir_in, ['S1*2015*VH*db.tif'])
-    srcfiles.extend(finder(dir_in, ['S1*2016*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*2017*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201801*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201802*VH*db.tif']))
-    srcfiles.extend(finder(dir_in, ['S1*201803*VH*db.tif']))
-    print(dstfile)
-    # check whether dstfile is already a file
-    if os.path.isfile(dstfile):
-        raise IOError('dstfile already exists')
-    
-    # create groups of similar time stamps for mosaicking.
-    # All images with a time stamp of less than 30s difference will be grouped
-    groups = groupbyTime(srcfiles, seconds, 30)
-
-    # final function call
-    # groups will be mosaicked first
-    # the resulting images will all have the same extent
-    stack(srcfiles=groups, dstfile=dstfile, resampling='bilinear',
-          targetres=[10, 10], srcnodata=-99, dstnodata=-99,
-          shapefile=shp, sortfun=seconds, separate=sep, overwrite=False)
-
-
-if __name__ == '__main__':
-    main()
-
-
-
 
